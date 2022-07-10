fbb9c7e92699bbba5b4204054b2df2f6446b2da5
Satchee <s.shastree@student.saedubai.com>
2022-07-10 14:38:47 +0400

    combined mechanics for class build
    -changed prefabs for cards
    -added batch renaming tool (:/)
    -merged card movement with ability to hold cards in hand

--- studio4/Assets/Scripts/Damage.cs
+++ studio4/Assets/Scripts/Damage.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 public class Damage : MonoBehaviour
 {
     //card functions
-    public HealthSystem healthSystem;
+    public PlayerManager healthSystem;
     public Card opponentCard;
     [SerializeField] Slider opponentCardSlider;
     [SerializeField] Text status;
 
     private void Start()
     {
-        opponentCardSlider.maxValue = opponentCard.health;
+        //opponentCardSlider.maxValue = opponentCard.health;
     }
 
     public void PlayerAttack(Card card)
