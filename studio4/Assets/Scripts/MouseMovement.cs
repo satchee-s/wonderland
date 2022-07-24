16e582e46bd88399a5e43b9d6a58b9328ebea212
Satchee <s.shastree@student.saedubai.com>
2022-07-24 13:58:01 +0400

    edited the game manager
    - made some changes to the game manager so it works better
    - added drag and drop to all cards

--- studio4/Assets/Scripts/MouseMovement.cs
+++ studio4/Assets/Scripts/MouseMovement.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class MouseMovement : MonoBehaviour
 {
     Camera cam;
-    public float yDistFromTable = 1f;
+    public float yDistFromTable;
     public bool canBeMoved = true;
 
     private void Start()
     {
         cam = Camera.main;
+        yDistFromTable = transform.position.y;
     }
 
     private void OnMouseDrag()
     {
-        if (canBeMoved)
+        if (canBeMoved && gameObject.tag == "Selectable")
         {
             Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.WorldToScreenPoint(transform.position).z);
             Vector3 newPos = cam.ScreenToWorldPoint(screenPos);
