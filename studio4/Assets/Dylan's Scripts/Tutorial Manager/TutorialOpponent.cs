4699a5437ebca1d38207bc7270e4a8cd86e92adf
dylansuarisSAE <dylansuaris@gmail.com>
2022-08-01 00:05:35 +0400

    Finished Tutorial Scene (UI), still need to add some code to specific indexes to completely finish TutorialManager, ALSO Added Opponent to Scene
    Added Opponent
    Redid Tutorial Scene

---
+++ studio4/Assets/Dylan's Scripts/Tutorial Manager/TutorialOpponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialOpponent : GameManager
+{
+    public List<Card> TutorialDeck = new List<Card>();
+    new void Start()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            handCards.Add(TutorialDeck[Random.Range(0, TutorialDeck.Count)]);
+            handCards[i].tag = "Selectable";
+            TutorialDeck.Remove(handCards[i]);
+
+            handCards[i].transform.position = cardSlots[i].transform.position;
+        }
+    }
+}
